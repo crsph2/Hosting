68afed2497add817ad40dd840a606b3ae953f4e0
crsph2 <crsph2@gmail.com>
2026-08-21 14:00:59 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-// ================================================================
-//  MATHQUEST - LÓGICA COMPLETA (VERSIÓN FINAL CON TUS DATOS)
-// ================================================================
-
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDWiRZWAU9sJMat1XsYznKqC4JxIXimL4c",
   authDomain: "jmate-54676.firebaseapp.com",
