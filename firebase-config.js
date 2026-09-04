9e31e4b69ee9a1d60ba9800be7a44a9ac94df171
crsph2 <crsph2@gmail.com>
2026-09-04 18:17:42 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,14 @@
-// Tu configuración de Firebase
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCrZVGH2lU_AzUTNAldgx92jRU0goeeKE0",
-  authDomain: "mate2-67072.firebaseapp.com",
-  projectId: "mate2-67072",
-  storageBucket: "mate2-67072.firebasestorage.app",
-  messagingSenderId: "563223996556",
-  appId: "1:563223996556:web:f4a982536abbdbe196f3a9",
-  measurementId: "G-QYYCEV58RN"
+  apiKey: "AIzaSyCxNI_HWssg9gtAoNxCULxZupzZSWmYXv4",
+  authDomain: "pruebas-1ac9e.firebaseapp.com",
+  projectId: "pruebas-1ac9e",
+  storageBucket: "pruebas-1ac9e.firebasestorage.app",
+  messagingSenderId: "418230723139",
+  appId: "1:418230723139:web:bda284b7da4b8ad9256af6",
+  measurementId: "G-5D65K25VKQ"
 };
 
-// Inicializamos Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Obtenemos la instancia de Firestore
-const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
